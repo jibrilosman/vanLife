cf9584c23bf9d5dc6bdf21e4a0055ee0736baaf7
jibrilosman <jibrilosman404@gmail.com>
2023-12-30 15:48:34 -0500

    modified footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,6 @@ const Footer = () => {
     const currentYear = new Date().getFullYear();
   return (
     <footer>
-      <p>Created by <a href="">@jessicamvs</a></p>
       <p>&copy; {currentYear} #vanlife</p>
     </footer>
   );
